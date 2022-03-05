bc22aef7eca82edeb6694585122b248096b46514
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-03-05 21:54:46 +0600

    counter app

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    text = request.GET.get('words')
+    if text == None:
+        text = ' '
+    number_of_words = len(text.split())
+    print(text)
+    return render(request, 'base/index.html', {'number_of_words': number_of_words})
